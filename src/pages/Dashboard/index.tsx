609afe9291b97d60e7603f1cd17f65d1b9d425cf
Flávio Arêas <vflavio9@gmail.com>
2021-09-30 09:04:38 -0300

    feat: fetch foods and categories to Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,129 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useCallback, useEffect, useState } from 'react';
 import Input from '../../components/Input';
 
-import { Container, Header } from './styles';
+import api from '../../services/api';
 
-const Dashboard: React.FC = () => (
-  <Container>
-    <Header>
-      <Input icon="search" placeholder="What food are you looking for?" />
-    </Header>
-  </Container>
-);
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  CategoriesListContainer,
+  CategoriesList,
+  CategoryContainer,
+  CategoryImage,
+  CategoryTitle,
+  FoodsListContainer,
+  FoodsList,
+  FoodContainer,
+  FoodImage,
+  FoodInfo,
+  FoodName,
+  FoodDescription,
+  FoodPrice,
+} from './styles';
+
+export interface Food {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  thumbnail_url: string;
+  formattedPrice: string;
+}
+
+export interface Category {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const [selectedCategory, setSelectedCategory] = useState<
+    number | undefined
+  >();
+
+  useEffect(() => {
+    async function loadFoods(): Promise<void> {
+      const response = await api.get('/foods');
+
+      setFoods(response.data);
+    }
+
+    loadFoods();
+  }, []);
+
+  useEffect(() => {
+    async function loadCategories(): Promise<void> {
+      const response = await api.get('/categories');
+
+      setCategories(response.data);
+    }
+
+    loadCategories();
+  }, []);
+
+  const handleSelectCategory = useCallback((id: number) => {
+    setSelectedCategory(id);
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Input icon="search" placeholder="What food are you looking for?" />
+      </Header>
+
+      <Content>
+        <Title>Categories</Title>
+        <CategoriesListContainer>
+          <CategoriesList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={categories}
+            keyExtractor={category => String(category.id)}
+            renderItem={({ item: category }) => (
+              <CategoryContainer
+                selected={category.id === selectedCategory}
+                onPress={() => handleSelectCategory(category.id)}
+              >
+                <CategoryImage source={{ uri: category.image_url }} />
+                <CategoryTitle selected={category.id === selectedCategory}>
+                  {category.title}
+                </CategoryTitle>
+              </CategoryContainer>
+            )}
+          />
+        </CategoriesListContainer>
+
+        <Title>Foods</Title>
+        <FoodsListContainer>
+          <FoodsList
+            showsHorizontalScrollIndicator={false}
+            data={foods}
+            keyExtractor={food => String(food.id)}
+            renderItem={({ item: food }) => (
+              <FoodContainer
+                selected={food.id === selectedCategory}
+                onPress={() => handleSelectCategory(food.id)}
+              >
+                <FoodImage source={{ uri: food.thumbnail_url }} />
+
+                <FoodInfo>
+                  <FoodName>{food.name}</FoodName>
+                  <FoodDescription>{food.description}</FoodDescription>
+                  <FoodPrice>{food.formattedPrice}</FoodPrice>
+                </FoodInfo>
+              </FoodContainer>
+            )}
+          />
+        </FoodsListContainer>
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,10 +1,20 @@
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
+
+import { Category, Food } from './index';
+
+interface CategoryContainerProps {
+  selected: boolean;
+}
+
+interface CategoryTitleProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-
-  /* padding: 0 24px; */
+  align-items: flex-start;
 
   background: #fff;
 `;
@@ -17,3 +27,57 @@ export const Header = styled.View`
 
   background: #1e90ff;
 `;
+
+export const Content = styled.View`
+  flex: 1;
+
+  padding: 0 24px;
+`;
+
+export const Title = styled.Text`
+  margin-top: 48px;
+
+  color: #0052a3;
+  font-family: 'Poppins-Bold';
+  font-size: 32px;
+`;
+
+export const CategoriesListContainer = styled.View`
+  height: 112px;
+`;
+
+export const CategoriesList = styled(
+  FlatList as new () => FlatList<Category>
+)``;
+
+export const CategoryContainer = styled(RectButton)<CategoryContainerProps>``;
+
+export const CategoryImage = styled.Image`
+  height: 64px;
+  width: 64px;
+`;
+
+export const CategoryTitle = styled.Text<CategoryTitleProps>``;
+
+export const FoodsListContainer = styled.View`
+  flex: 1;
+`;
+
+export const FoodsList = styled(FlatList as new () => FlatList<Food>)``;
+
+export const FoodContainer = styled(RectButton)<CategoryContainerProps>`
+  flex-direction: row;
+`;
+
+export const FoodImage = styled.Image`
+  height: 64px;
+  width: 64px;
+`;
+
+export const FoodInfo = styled.View``;
+
+export const FoodName = styled.Text``;
+
+export const FoodDescription = styled.Text``;
+
+export const FoodPrice = styled.Text``;
